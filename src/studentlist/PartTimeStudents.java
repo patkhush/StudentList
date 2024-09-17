9d717e072a5c7a170cc1abca1adce17e07c6247f
Khush Patel <pakhushn@sheridancollege.ca>
2024-09-17 09:36:06 -0400

    Part time Students aaded

---
+++ src/studentlist/PartTimeStudents.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author khush
+ */
+public class PartTimeStudents {
+    
+    private int numofcourses;
+
+    public int getNumofcourses() {
+        return numofcourses;
+    }
+
+    public void setNumofcourses(int numofcourses) {
+        this.numofcourses = numofcourses;
+    }
+    
+}
